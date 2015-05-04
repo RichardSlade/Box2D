3a29d0f5c8b298536cf26dc0a82c06204700e40c
Richard Slade <richard.slade@coderus.com>
2015-05-04 13:28:06 +0100

    General(main.cpp): Added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <Box2D/Box2D.h>
+
+
+int main()
+{
+   int i = 0;
+
+   std::cout << i << std::endl;
+
+   return 0;
+}
